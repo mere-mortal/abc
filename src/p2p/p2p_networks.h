8d897c6ac359bcaaa26a43ff003a3e6917294e13
mere-mortal <dean@unwined.co.za>
2014-08-17 12:27:22 +0200

    new

---
+++ src/p2p/p2p_networks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2014 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace nodetool
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xA4 ,0x2A, 0xA4, 0xA6 , 0x4A, 0x1A , 0xA4, 0x1A, 0xA9, 0xA2, 0x3A, 0xA3, 0x4A, 0xA1, 0x1A, 0x6A} };
+}
